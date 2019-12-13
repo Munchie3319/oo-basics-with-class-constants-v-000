04c7fa101bbcb53e8fa74728a0d80c60534b4092
rosa lee <rosalee0116@gmail.com>
2019-12-13 20:21:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,15 @@ BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.any? { |b| b == brand}
   end
 
-  def cobble
+
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
 end
